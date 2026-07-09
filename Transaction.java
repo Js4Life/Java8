4bf1ab31e80b05ebed330405c91f2c28528ccbe5
Raghavendra M <32561508+Js4Life@users.noreply.github.com>
2026-07-09 10:20:21 +0530

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,19 @@
+public class Transcation {
+
+      String date;
+      int amount;
+
+      public Transcation(String date,int amount) {
+        this.date= date;
+        this.amount = amount;
+      }
+
+      public String getDate() {
+        return date;
+      }
+
+      public int getAmount() {
+        return amount;
+      }
+
+}
